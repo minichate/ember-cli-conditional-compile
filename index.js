4be38c3066997dd67462de79743559dbed3a0aec
Jan Krutisch <jan@krutisch.de>
2022-09-09 19:31:15 +0200

    fix test failure

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const Funnel = require('broccoli-funnel');
-const merge = require('lodash.merge');
-const replace = require('broccoli-replace');
-const chalk = require('chalk');
-const VersionChecker = require('ember-cli-version-checker');
-const TemplateCompiler = require('./lib/template-compiler');
-const hash = require('object-hash');
-const fs = require('fs');
-const path = require('path');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
+const Funnel = require("broccoli-funnel");
+const merge = require("lodash.merge");
+const replace = require("broccoli-replace");
+const chalk = require("chalk");
+const VersionChecker = require("ember-cli-version-checker");
+const TemplateCompiler = require("./lib/template-compiler");
+const hash = require("object-hash");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-  name: 'ember-cli-conditional-compile',
+  name: "ember-cli-conditional-compile",
   enableCompile: false,
 
   init: function() {
     this._super.init && this._super.init.apply(this, arguments);
 
     const checker = new VersionChecker(this);
-    checker.for('ember-source').assertAbove('2.9.0');
+    checker.for("ember-source").assertAbove("2.9.0");
 
-    this.htmlbarsVersion = checker.for('ember-cli-htmlbars', 'npm');
-    this.uglifyVersion = checker.for('ember-cli-uglify', 'npm');
-    this.terserVersion = checker.for('ember-cli-terser', 'npm');
+    this.htmlbarsVersion = checker.for("ember-cli-htmlbars", "npm");
+    this.uglifyVersion = checker.for("ember-cli-uglify", "npm");
+    this.terserVersion = checker.for("ember-cli-terser", "npm");
   },
 
   included: function(app, parentAddon) {
-    this.readConfig()
+    this.readConfig();
 
-    const target = (parentAddon || app);
+    const target = parentAddon || app;
 
     let options = {
       options: {
@@ -38,46 +38,52 @@ module.exports = {
     };
 
     if (this.terserVersion.exists()) {
-      target.options = merge(target.options, { 'ember-cli-terser': { terser: options.options } });
-      this.enableCompile = target.options['ember-cli-terser'].enabled;
-    } else if (this.uglifyVersion.satisfies('>= 2.0.0')) {
-      target.options = merge(target.options, { 'ember-cli-uglify': { uglify: options.options } });
-      this.enableCompile = target.options['ember-cli-uglify'].enabled;
+      target.options = merge(target.options, {
+        "ember-cli-terser": { terser: options.options }
+      });
+      this.enableCompile = target.options["ember-cli-terser"].enabled;
+    } else if (this.uglifyVersion.satisfies(">= 2.0.0")) {
+      target.options = merge(target.options, {
+        "ember-cli-uglify": { uglify: options.options }
+      });
+      this.enableCompile = target.options["ember-cli-uglify"].enabled;
     } else {
       target.options.minifyJS = merge(target.options.minifyJS, options);
       this.enableCompile = target.options.minifyJS.enabled;
     }
 
     const templateCompilerInstance = {
-      name: 'conditional-compile-template',
+      name: "conditional-compile-template",
       plugin: TemplateCompiler(this._config.featureFlags)
-    }
+    };
 
-    if (this.htmlbarsVersion.satisfies('>= 1.3.0')) {
-      templateCompilerInstance['baseDir'] = function() {
+    if (this.htmlbarsVersion.satisfies(">= 1.3.0")) {
+      templateCompilerInstance["baseDir"] = function() {
         return __dirname;
       };
 
-      const featureFlags = this._config.featureFlags
+      const featureFlags = this._config.featureFlags;
 
-      templateCompilerInstance['cacheKey'] = function() {
+      templateCompilerInstance["cacheKey"] = function() {
         return hash(featureFlags);
       };
     } else {
-      console.log(chalk.yellow(
-        'Upgrade to ember-cli-htmlbars >= 1.3.0 to get build caching'
-      ));
+      console.log(
+        chalk.yellow(
+          "Upgrade to ember-cli-htmlbars >= 1.3.0 to get build caching"
+        )
+      );
     }
 
-    target.registry.add('htmlbars-ast-plugin', templateCompilerInstance);
+    target.registry.add("htmlbars-ast-plugin", templateCompilerInstance);
   },
 
   readConfig() {
     const root = this.project.root;
-    const config = this.project.config(EmberApp.env())
-    const flagsEnv = config.featureFlagsEnvironment || EmberApp.env()
+    const config = this.project.config(EmberApp.env());
+    const flagsEnv = config.featureFlagsEnvironment || EmberApp.env();
 
-    let configFactory = path.join(root, 'config', 'feature-flags.js');
+    let configFactory = path.join(root, "config", "feature-flags.js");
 
     if (fs.existsSync(configFactory)) {
       this._config = Object.assign({}, require(configFactory)(flagsEnv));
@@ -87,9 +93,9 @@ module.exports = {
   },
 
   setupPreprocessorRegistry: function(type, registry) {
-    registry.add('js', {
-      name: 'ember-cli-conditional-compile',
-      ext: 'js',
+    registry.add("js", {
+      name: "ember-cli-conditional-compile",
+      ext: "js",
       toTree: (tree) => this.transpileTree(tree)
     });
   },
@@ -99,58 +105,65 @@ module.exports = {
    * removes the code non reachable.
    */
   transpileTree(tree) {
-    const esTranspiler = require('broccoli-babel-transpiler');
-    const inlineFeatureFlags = require('babel-plugin-inline-replace-variables');
+    const esTranspiler = require("broccoli-babel-transpiler");
+    const inlineFeatureFlags = require("babel-plugin-inline-replace-variables");
     if (!this.enableCompile) {
       return tree;
     }
     return esTranspiler(tree, {
-      plugins: [
-        [inlineFeatureFlags, this._config.featureFlags]
-      ]
+      plugins: [[inlineFeatureFlags, this._config.featureFlags]]
     });
   },
 
   postprocessTree: function(type, tree) {
-    if (type !== 'js') return tree;
+    if (type !== "js") return tree;
 
     let config = this.project.config(EmberApp.env());
 
     if (!this._config.featureFlags) {
-      console.log(chalk.red(
-        'Could not find any feature flags.' +
-        'You may need to add them in your config/environment.js'
-      ));
+      console.log(
+        chalk.red(
+          "Could not find any feature flags." +
+            "You may need to add them in your config/environment.js"
+        )
+      );
       return tree;
     }
 
     let excludes = [];
 
-    Object.keys(this._config.featureFlags).map(function(flag) {
-      if (this._config.includeDirByFlag && !this._config.featureFlags[flag] && this._config.includeDirByFlag[flag]) {
-        excludes = excludes.concat(this._config.includeDirByFlag[flag]);
-      }
-    });
+    if (this._config.featureFlags) {
+      Object.keys(this._config.featureFlags).map(function(flag) {
+        if (
+          this._config.includeDirByFlag &&
+          !this._config.featureFlags[flag] &&
+          this._config.includeDirByFlag[flag]
+        ) {
+          excludes = excludes.concat(this._config.includeDirByFlag[flag]);
+        }
+      }, this);
+    }
 
     if (this.enableCompile) {
-      excludes = excludes.concat(
-        /ember-cli-conditional-compile-features.js/
-      );
+      excludes = excludes.concat(/ember-cli-conditional-compile-features.js/);
     } else {
       tree = replace(tree, {
-        files: [config.modulePrefix + '/initializers/ember-cli-conditional-compile-features.js'],
-        patterns: [{
-          match: /EMBER_CLI_CONDITIONAL_COMPILE_INJECTIONS/g,
-          replacement: JSON.stringify(this._config.featureFlags || {})
-        }]
+        files: [
+          config.modulePrefix +
+            "/initializers/ember-cli-conditional-compile-features.js"
+        ],
+        patterns: [
+          {
+            match: /EMBER_CLI_CONDITIONAL_COMPILE_INJECTIONS/g,
+            replacement: JSON.stringify(this._config.featureFlags || {})
+          }
+        ]
       });
     }
 
     return new Funnel(tree, {
       exclude: excludes,
-      description: 'Funnel: Conditionally Filtered App'
+      description: "Funnel: Conditionally Filtered App"
     });
   }
-
-
 };
